5bd1790bea960677bd93fc6ba86918b2915e043c
junwan kim <jkim663@myseneca.ca>
2022-10-05 12:39:17 -0400

    initial commit

--- server.js
+++ server.js
@@ -23,15 +23,46 @@ var express = require("express");
 var app = express();
 var path = require('path');
 app.use(express.static('img'));
+const multer = require("multer");
+
+
+app.use(express.static('public'));
 
 var blogService = require("./blog-service.js");
-
 
 onHttpStart = () => {
     console.log('Express http server listening on port ' + HTTP_PORT);
 }
 
-app.use(express.static('public'));
+const imgPath = "/public/images/uploaded";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, imgPath))
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/images/add", upload.single("imageFile"), function (req, res) {
+    res.redirect("/images");
+});
+
+app.get("/images", function (req, res) {
+    fs.readdir(path.join(__dirname, imgPath), function (err, items) {
+  
+      var obj = { images: [] };
+      var size = items.length;
+      for (var i = 0; i < items.length; i++) {
+        obj.images.push(items[i]);
+      }
+      res.json(obj);
+    });
+});
+  
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/home.html"));
@@ -46,6 +77,14 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
+app.get("/students/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/addStudents.html"));
+});
+
+app.get("/images/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/addImages.html"));
+});
+  
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname,"views/error404.html"));
 });
